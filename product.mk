362a876028546da8e8366c152912e1d9b721a7b2
a3project <aepranata@gmail.com>
2022-07-29 05:49:04 +0700

    overlay: Initialize Updater

---
+++ product.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Overlays
+PRODUCT_PACKAGES += \
+    RomUpdaterOverlay
+
+# Updater
+PRODUCT_PACKAGES += \
+    Updater
